622683b9261195ff3c6efb27a97d1aa94022c730
Ariful-Islam-Shakil <arif.cse18cuet@gmail.com>
2025-09-22 03:15:09 +0600

    View all Products

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,64 +1,16 @@
-'use client';
-import Login from '@/components/auth/Login';
-import GetCarts from '@/components/cart/GetCarts';
-import AddProduct from '@/components/products/AddProduct';
-import AllProducts from '@/components/products/AllProducts';
-import RegisterUser from '@/components/RegisterUser';
-import User from '@/components/User';
-import Users from '@/components/Users';
-import { useRepo } from '@/context/RepoContext';
-import { clearCart } from '@/lib/cart_routes';
+'use client'
+import AllProducts from '@/components/products/AllProducts'
+// import ProductCart from '@/components/products/ProductCart'
 import React from 'react'
 
 const page = () => {
-  const {temp, setTemp, accessToken} = useRepo();
-  const [getCurtStatus, setGetCartStatus] = React.useState(false);
-  const handleClearCart = async ()=>{
-
-    try{
-      const response = await clearCart(accessToken);
-    }catch(error){
-      console.log("error clearing cart: ", error)
-    }
-  };
-  
   return (
-    <div>
-      <div className='border p-2 rounded-lg'> Login First
-        <hr/>
-        <Login/>
-        <hr/>
-      </div>
-      <button
-        className='bg-blue-500 text-white p-2 rounded-lg m-2'
-        onClick={()=>{
-          setGetCartStatus(!getCurtStatus);}}
-        >
-        Get Carts
-      </button>
-      {getCurtStatus && (
-        <div>
-          <button
-            className='bg-red-500 text-white p-2 rounded-lg m-2'
-            onClick={handleClearCart}
-          >Clear cart</button>
-
-          <GetCarts/>
-        </div>
-      )}
+<div >
+  <AllProducts/>
  
-      <br/>
-      <hr/>
-      <hr/>
-      
-      page: {temp} <br/>
-      <Users/>
-      <User/>
-      <RegisterUser/>
-      <AddProduct/>
-      <AllProducts/>
-    
-    </div>
+
+</div>
   )
 }
+
 export default page
--- frontend/components/products/AllProducts.js
+++ frontend/components/products/AllProducts.js
@@ -2,6 +2,7 @@ import { useRepo } from '@/context/RepoContext';
 import { addToCart } from '@/lib/cart_routes';
 import { listProducts } from '@/lib/product_routes';
 import React, { useEffect, useState } from 'react'
+import ProductCart from './ProductCart';
 
 const AllProducts = () => {
     const [productsList, setProductsList] = useState([]);
@@ -32,21 +33,23 @@ const AllProducts = () => {
     }
 
   return (
-    <div>
+    <div className="mt-6 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {productsList.map((product) => (
-            <div key={product.id} className='border-2 m-2 p-2'>
-                <ul>
-                    <li>Product Name: {product.name}</li>
-                    <li>Price: ${product.price}</li>
-                    <li>Description: {product.description}</li>
-                    <button 
-                        className='bg-blue-500 text-white p-2 rounded-lg mt-2'
-                        onClick={() => handleAddToCart(product.id)}
-                    >
-                        Add to Cart
-                    </button>
-                </ul>
-            </div>
+            // <div key={product.id} className='border-2 m-2 p-2'>
+            //     <ul>
+            //         <li>Product Name: {product.name}</li>
+            //         <li>Price: ${product.price}</li>
+            //         <li>Description: {product.description}</li>
+            //         <button 
+            //             className='bg-blue-500 text-white p-2 rounded-lg mt-2'
+            //             onClick={() => handleAddToCart(product.id)}
+            //         >
+            //             Add to Cart
+            //         </button>
+            //     </ul>
+                
+            // </div>
+            <ProductCart key={product.id} product={product} handleAddToCart={handleAddToCart}/>
                     ))};
     </div>
   )
